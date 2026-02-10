c9c572ca26c75a1b215ceeedc5778aacf877e580
Baldemar Estrada <Baldemar.estrada@fullman.cloud>
2026-02-10 11:18:09 -0600

    Publish to Exchange with RAML file Test v1.1

--- src/main/java/com/coppel/exchangeMuleJava/controller/HelloController.java
+++ src/main/java/com/coppel/exchangeMuleJava/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World with Exchange";
+        return "Hello World with Exchange v1.2";
     }
 }
