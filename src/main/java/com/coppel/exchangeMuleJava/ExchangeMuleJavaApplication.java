e25f2eff10e41190c05372a6c4582db01700d192
Baldemar Estrada <Baldemar.estrada@fullman.cloud>
2026-02-10 10:23:32 -0600

    Repo to test publish with Exchange

---
+++ src/main/java/com/coppel/exchangeMuleJava/ExchangeMuleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.coppel.exchangeMuleJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeMuleJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeMuleJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coppel/exchangeMuleJava/ExchangeMuleJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coppel.exchangeMuleJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeMuleJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
